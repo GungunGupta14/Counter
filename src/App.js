540ed9e3e3d2e3fa3ab20df7749c8e33cc351d7d
Gungun Gupta <gungungupta1404@gmail.com>
2023-04-25 18:34:04 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div>
-      <div>Increment and Decrement</div>
-      <div>
-        <button>-</button>
-        <div></div>
-        <button>+</button>
-      </div>
-      <button>reset</button>
-    </div>
-  );
+    return ( 
+      <div >
+        <div className="w-[100vw] h-[100vh] flex justify-ccntre items-cent"> Increment and Decrement </div> 
+        <div>
+        <button> - < /button> 
+        <div  </div> 
+        <button> + </button> 
+        </div> 
+        <button > reset </button> 
+        </div >
+    );
 }
 
 export default App;
