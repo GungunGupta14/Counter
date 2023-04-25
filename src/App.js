b9ff32fdfb3f2f50e40ab49df514337a8f32ef49
Gungun Gupta <gungungupta1404@gmail.com>
2023-04-25 18:29:25 +0530

    .

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div>Increment and Decrement</div>
+      <div>
+        <button>-</button>
+        <div></div>
+        <button>+</button>
+      </div>
+      <button>reset</button>
+    </div>
+  );
+}
+
+export default App;
